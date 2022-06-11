cadbb6f5de31b03d40994a9455a4f43eb055b427
Jannis Bautsch <jannis.bautsch@softgarden.de>
2022-06-11 12:35:13 +0200

    add court controller

--- app/src/main/java/com/example/courtfinder/control/CourtController.java
+++ app/src/main/java/com/example/courtfinder/control/CourtController.java
@@ -1,4 +1,26 @@
 package com.example.courtfinder.control;
 
-public class CourtController {
+import com.example.courtfinder.model.Court;
+
+public class CourtController implements ICourtController{
+
+    private Court court;
+
+    @Override
+    public Court newCourt(String name, float lat, float lon ) {
+        this.court = new Court(name, lat, lon);
+        return this.court;
+    }
+
+    @Override
+    public Court[] getAllCourts() {
+        //api call
+        return new Court[0];
+    }
+
+    @Override
+    public void saveCourt() {
+        //api call
+        //return bool?
+    }
 }
--- app/src/main/java/com/example/courtfinder/control/ICourtController.java
+++ app/src/main/java/com/example/courtfinder/control/ICourtController.java
@@ -1,4 +1,13 @@
 package com.example.courtfinder.control;
 
+import com.example.courtfinder.model.Court;
+
 public interface ICourtController {
+
+    Court newCourt(String name, float lat, float lon );
+
+    Court[] getAllCourts();
+
+    void saveCourt();
+
 }
