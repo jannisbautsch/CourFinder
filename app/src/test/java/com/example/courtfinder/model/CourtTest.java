8f6cf4cc9ee1ba0046c8b857bc104cfb23f4b960
Jannis Bautsch <jannis.bautsch@softgarden.de>
2022-07-11 12:25:26 +0200

    add court model test

--- app/src/test/java/com/example/courtfinder/model/CourtTest.java
+++ app/src/test/java/com/example/courtfinder/model/CourtTest.java
@@ -1,17 +1,40 @@
 package com.example.courtfinder.model;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
 
-@RunWith(RobolectricTestRunner.class)
 public class CourtTest {
 
     @Test
-    public void getId(){
-
+    public void getsCorrectId(){
+        Court court = new Court("1234", "Cool Court", 53.123, 13.123);
+        String id = court.getId();
+        assertEquals(id, "1234");
     };
+
+    @Test
+    public void getsCorrectName(){
+        Court court = new Court("1234", "Cool Court", 53.123, 13.123);
+        String name = court.getName();
+        assertEquals(name, "Cool Court");
+    };
+
+    @Test
+    public void getsCorrectLat(){
+        Court court = new Court("1234", "Cool Court", 53.123, 13.123);
+        double lat = court.getLat();
+        assertEquals(lat, 53.123, 0);
+    };
+
+
+    @Test
+    public void getsCorrectLon(){
+        Court court = new Court("1234", "Cool Court", 53.123, 13.123);
+        double lon = court.getLon();
+        assertEquals(lon, 13.123, 0);
+    };
+
 
 }
