9cea1faee152a34f661b2f2e316188264443932e
Nils.Rausch <Nils.Rausch@exxeta.com>
2022-07-11 15:06:23 +0200

    Added missing Interface Descriptions

--- app/src/main/java/com/example/courtfinder/View/ILoginView.java
+++ app/src/main/java/com/example/courtfinder/View/ILoginView.java
@@ -2,6 +2,15 @@ package com.example.courtfinder.View;
 
 public interface ILoginView {
 
+    /**
+     * Redirects on Valid
+     * @param message
+     */
     void OnLoginSuccess(String message);
+
+    /**
+     *  Redirects on Error
+     * @param message
+     */
     void OnLoginError(String message);
 }
--- app/src/main/java/com/example/courtfinder/model/IUser.java
+++ app/src/main/java/com/example/courtfinder/model/IUser.java
@@ -3,7 +3,24 @@ package com.example.courtfinder.model;
 
 public interface IUser {
 
+    /**
+     * returns Value of the Email
+     *
+     * @return String
+     */
     String getEmail();
+
+    /**
+     * returns Value of the Password
+     *
+     * @return String
+     */
     String getPassword();
+
+    /**
+     * returns Int Code for Validation
+     *
+     * @return Int
+     */
     int isValid();
 }
