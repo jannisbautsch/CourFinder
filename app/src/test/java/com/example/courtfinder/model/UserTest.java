02813b915914cbd2ca7b35b6c23070b35597d7cc
Jannis Bautsch <jannis.bautsch@softgarden.de>
2022-07-11 12:14:17 +0200

    add unit test for user model

--- app/src/test/java/com/example/courtfinder/model/UserTest.java
+++ app/src/test/java/com/example/courtfinder/model/UserTest.java
@@ -8,10 +8,40 @@ import org.robolectric.RobolectricTestRunner;
 
 @RunWith(RobolectricTestRunner.class)
 public class UserTest {
+    
+    @Test
+    public void emailValidator_EmailIsEmpty() {
+        User user =  new User("", "courtfinder123");
+        int loginCode = user.isValid();
+        assertEquals(loginCode, (0));
+    }
 
     @Test
-    public void emailValidator_CorrectEmailSimple() {
-        User user =  new User("admin@counterfinder.de", "courtfinder123");
+    public void emailValidator_NotValidEmailAdress() {
+        User user =  new User("admin", "courtfinder123");
+        int loginCode = user.isValid();
+        assertEquals(loginCode, (1));
+    }
+
+
+    @Test
+    public void emailValidator_PasswordIsEmpty() {
+        User user =  new User("hello@courtfinder.com", "");
+        int loginCode = user.isValid();
+        assertEquals(loginCode, (2));
+    }
+
+
+    @Test
+    public void emailValidator_PasswordToShort() {
+        User user =  new User("hello@courtfinder.com", "court");
+        int loginCode = user.isValid();
+        assertEquals(loginCode, (3));
+    }
+
+    @Test
+    public void emailValidator_CorrectEmail() {
+        User user =  new User("hello@courtfinder.com", "courtfinder123");
         int loginCode = user.isValid();
         assertEquals(loginCode, (-1));
     }
