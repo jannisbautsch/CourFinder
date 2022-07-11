95be8fef4da7152b90e2e197c72d03d772ab1cea
Nils.Rausch <Nils.Rausch@exxeta.com>
2022-07-11 11:55:51 +0200

    Unit Test User

---
+++ app/src/test/java/com/example/courtfinder/model/UserTest.java
@@ -0,0 +1,17 @@
+package com.example.courtfinder.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class UserTest {
+
+    @Test
+    public void emailValidator_CorrectEmailSimple() {
+        User user =  new User("admin@counterfinder.de", "courtfinder123");
+        int loginCode = user.isValid();
+        assertEquals(loginCode, (-1));
+    }
+
+}
