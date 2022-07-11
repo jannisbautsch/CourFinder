92f8aa744f0d3e1bf8d1a180cb2c7e3bc15d77fa
Nils.Rausch <Nils.Rausch@exxeta.com>
2022-07-11 10:52:20 +0200

    Löppt der Lachsingers

--- app/src/main/java/com/example/courtfinder/LogInActivity.java
+++ app/src/main/java/com/example/courtfinder/LogInActivity.java
@@ -2,6 +2,7 @@ package com.example.courtfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -39,6 +40,8 @@ public class LogInActivity extends AppCompatActivity implements ILoginView {
     @Override
     public void OnLoginSuccess(String message) {
         Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     @Override
